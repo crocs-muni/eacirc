ce001e2e09c0211950ee3ca95a50c60b4faf27e9
Martin Ukrop <mukrop@mail.muni.cz>
2013-02-15 18:50:20 +0100

    utilities added

---
+++ utils/boinc_downloader.sh
@@ -0,0 +1,59 @@
+#! /bin/bash
+
+VERSION=1.2
+PARENTDIR=../../boinc/_processed
+BASEHTML=http://centaur.fi.muni.cz:8000/
+
+echo "Results downloader for LaBAK@BOINC, version "$VERSION
+if [ $# -eq 0 ]
+then 
+	echo "usage: "$0" <results-html-file> [<results-html-file> ...]"
+	echo
+	echo " - provide one or more html files from LaBAK@BOINC/results"
+	echo " - downloads all files provided in BOINC results table"
+	echo " - only files for successfull runs are downloaded"
+	echo " - downloaded jobs are storer in "$PARENTDIR" (this can be changes in script header)"
+	echo " - dependency: html-xml-utils"
+	exit -1
+fi
+if [ ! -d $PARENTDIR ]
+then
+	mkdir -p $PARENTDIR
+fi
+
+# main loop for given source files
+for SOURCEHTML in "$@"
+do
+	if [ ! -f $SOURCEHTML ]
+	then
+		echo "File \""$SOURCEHTML"\" does not exist."
+		continue		
+	fi
+	echo "Downloading results from LaBAK@BOINC for source html: "$SOURCEHTML
+	CLEAN=$SOURCEHTML.clean
+	# clean HTML
+	hxclean $SOURCEHTML >$CLEAN
+	ITEM=0
+	while true 
+	do
+		ITEM=$[$ITEM + 1]
+		DIR=`cat $CLEAN | hxselect .line-ok:nth-child\($ITEM\) td:nth-child\(2\) a | sed ':a;N;$!ba;s/\n/ /g' | sed 's/[^>]*>[ \t]*\([^<> \t]*\)[ \t]*<\/[^<]*/\1/' | sed 's/\([^[]*\)\[\(.*\)\]/\1\/\2/'`
+		if [ -z $DIR ]; then break; fi
+		DIR=$PARENTDIR/$DIR
+		echo =\> Processing $DIR
+		mkdir -p $DIR
+		ROW=`cat $CLEAN | hxselect .line-ok:nth-child\($ITEM\) td:nth-child\(13\) a`
+		FILES=`echo $ROW | sed 's/<a[^>]*>\([^<>]*\)<\/a>/\1 /g'`
+		for FILE in $FILES
+		do
+			 echo $FILE
+			SED="s/.*<a\([^>]*\)>"$FILE"<\/a>.*/\1/"
+			URL1=`echo $ROW | sed $SED`
+			URL2=`echo $URL1 | sed 's/[^"]*"\([^"]*\)".*/\1/'`
+			 echo $BASEHTML$URL2
+			 echo $DIR/$FILE
+			# wget -q $BASEHTML$URL2 -O$DIR/$FILE
+		done
+	done
+	rm $CLEAN
+done
---
+++ utils/get_scores.hs
@@ -0,0 +1,141 @@
+import System.FilePath (pathSeparator)
+import System.Posix.Files (fileExist)
+import System.Environment (getArgs)
+import Data.List (isInfixOf)
+import Control.Monad (foldM)
+
+scoreFilename :: String
+scoreFilename = "scores.log"
+
+logFilename :: String
+logFilename = "eacirc.log"
+
+resultsFilename :: String
+resultsFilename = "results.txt"
+
+testVectorChangeFreq :: Int
+testVectorChangeFreq = 100
+
+stableGenTheshold :: Float
+stableGenTheshold = 0.99
+
+maxRuns :: Int
+maxRuns = 30
+
+logSuspects :: [String]
+logSuspects = ["warning","error"]
+
+data GenerationInfo = Info {
+  generation :: Int,
+  avgFit :: Float,
+  maxFit :: Float,
+  minFit :: Float
+} deriving (Show, Eq)
+
+type Scores=[GenerationInfo]
+
+data RunStats = Stats {
+  errors :: [String],
+  stableGen :: [Int],
+  avgTable :: [String]
+} deriving (Show, Eq)
+
+emptyRunStats :: RunStats
+emptyRunStats = Stats{errors=[], stableGen=[], avgTable=[]}
+
+foldRunStats :: String -> RunStats -> Int -> IO RunStats
+foldRunStats path stats run = do
+  newStats <- processRun path run
+  return Stats{errors=(concatMap errors [stats,newStats]), 
+               stableGen=(concatMap stableGen [stats,newStats]),
+               avgTable=(concatMap avgTable [stats,newStats])}
+
+processRun :: String -> Int -> IO RunStats
+processRun path run = do
+  fileExist (path ++ show run) >>= \exists -> if exists 
+    then do
+      errors <- checkErrors path run
+      fileExist scoreFile >>= \exists -> if exists
+        then do
+          scores <- readFile scoreFile >>= return . parseScoreFile
+          return Stats{errors=errors, 
+                       stableGen=[getStableGen scores], 
+                       avgTable=[getAvgTable run scores]}
+        else
+          return Stats{errors=errors, stableGen=[], avgTable=[show run ++ "\tscoreFile does not exist!"]}
+    else return Stats{errors=["Run " ++ show run ++ " does not exist!"],
+                      stableGen=[],
+                      avgTable=[]}
+  where scoreFile = path ++ show run ++ (pathSeparator : scoreFilename)
+
+parseScoreFile :: String -> Scores
+parseScoreFile = zipWith build [1..] . map (take 4 . words) . lines
+  where build :: Int -> [String] -> GenerationInfo
+        build g [_,a,ma,mi] = Info{generation=g, avgFit=(read a), maxFit=(read ma), minFit=(read mi)}
+        build _ x = error (show x)
+
+filterScores :: [GenerationInfo] -> [GenerationInfo]
+filterScores = filter ((== 1) . (flip mod testVectorChangeFreq) . generation)
+
+getAverages :: [GenerationInfo] -> [Float]
+getAverages info = map makeAvg [avgFit, maxFit, minFit]
+  where makeAvg :: (GenerationInfo -> Float) -> Float
+        makeAvg f = average $ map f info
+
+getAvgTable :: Int -> Scores -> String
+getAvgTable run scores = 
+  formatResults . getAverages $ filterScores scores
+    where formatResults :: [Float] -> String
+          formatResults stats = show run ++ concatMap (('\t':). show) stats
+
+getStableGen :: Scores -> Int
+getStableGen scores =
+  generation $ if null stable then last scores else head stable 
+  where stable = until allBigger dropSmall $ filterScores scores
+        allBigger = all ((>= stableGenTheshold) . maxFit) . take 50
+        dropSmall = dropWhile (\info -> maxFit info < stableGenTheshold) . 
+                    dropWhile (\info -> maxFit info >= stableGenTheshold)
+
+checkErrors :: String -> Int -> IO [String]
+checkErrors path run = do
+  fileExist logFile >>= \exists -> if exists 
+    then readFile logFile >>= return . foldl findSuspects [] . lines
+    else return . (:[]) $ "Log file for run " ++ show run ++ " does not exist!"
+  where logFile = path ++ show run ++ [pathSeparator] ++ logFilename
+        findSuspects errors line = if any (flip isInfixOf line) logSuspects
+          then ("Suspect in run " ++ show run ++ ": " ++ line) : errors
+          else errors
+
+processFolder :: String -> IO ()
+processFolder path = do
+  fileExist path >>= \exists -> if exists 
+    then do
+      putStrLn $ "=> Processing folder " ++ path
+      stats <- foldM (foldRunStats path) emptyRunStats [1..maxRuns]
+      writeFile resultsFile $ unlines [resultsHeader, formatErrors (errors stats),
+                   formatStableGen (stableGen stats), formatTable (avgTable stats)]
+    else putStrLn $ "Folder " ++ path ++ " does not exist!"
+  where resultsFile = path ++ resultsFilename
+        resultsHeader = "EACirc - results for job " ++ job ++ "\n"
+        job = reverse . takeWhile (not . (==pathSeparator)) . tail $ reverse path
+        formatErrors stats = if null stats then "No error suspercts found.\n"
+                       else "Error suspects in this job:\n" ++ unlines stats
+        formatTable stats = tableHeader ++ "\n" ++ unlines stats
+        tableHeader = "note: Following table displays averages for all runs.\n"++
+                      "run\taverage\tmaximum\tminimum"
+        formatStableGen stats = if null stats then "No stable generation found.\n"
+                                else stableGenHeader ++ (unwords $ map show stats) ++
+                                     "\naverage: " ++ (show . average $ map fromIntegral stats) ++ "\n"
+        stableGenHeader = "Stable generations (after test vector change, "++
+                          "fit does not drop below " ++ show stableGenTheshold ++ " for " ++ 
+                          show (50 * testVectorChangeFreq) ++ " gens):\n"
+
+average :: Fractional a => [a] -> a
+average xs = sum xs / (fromIntegral $ length xs)
+
+iff :: Bool -> a -> a -> a
+iff c t e = if c then t else e
+
+main :: IO ()
+main = do
+  getArgs >>= mapM_ processFolder
