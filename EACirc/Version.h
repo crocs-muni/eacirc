75220767e1eccb2980f92c0f60180b099eb539df
Martin Ukrop <mukrop@mail.muni.cz>
2013-10-19 18:54:03 +0200

    commit for master (commit id separation)

--- EACirc/Version.h
+++ EACirc/Version.h
@@ -1,3 +1,3 @@
 // current git commit info (updated before build if correctly set-up) 
-#define GIT_COMMIT "6c01dd16463923158418b764e05bfa4b6eaf7bd6"
-#define GIT_COMMIT_SHORT "6c01dd1"
+#define GIT_COMMIT "7207ccc6e255bb70e4b717e5b81741c3c4a592dc"
+#define GIT_COMMIT_SHORT "7207ccc"
