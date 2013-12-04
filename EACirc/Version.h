f53f4370bad19ecfcb24ef4c2fa309a695c675fb
Martin Ukrop <mukrop@mail.muni.cz>
2013-12-04 20:54:33 +0100

    BUGFIX removed "1-" from fitness computation

--- EACirc/Version.h
+++ EACirc/Version.h
@@ -1,3 +1,3 @@
-// current git commit info (updated before build if correctly set-up)
-#define GIT_COMMIT "537c46b6986b881a1c5c99eeb1a80ca6235ee524"
-#define GIT_COMMIT_SHORT "537c46b"
+// current git commit info (updated before build if correctly set-up) 
+#define GIT_COMMIT "a524e80ee533722f8314118638f0d25f8722a052"
+#define GIT_COMMIT_SHORT "a524e80"
