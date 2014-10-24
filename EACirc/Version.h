a50bc5afb5cf5539fd3405c46ed71a8e4ca44b63
Martin Ukrop <mukrop@mail.muni.cz>
2014-10-24 13:29:23 +0200

    Version.h with "n/a" commit hash

--- EACirc/Version.h
+++ EACirc/Version.h
@@ -1,3 +1,3 @@
 // current git commit info (updated before build if correctly set-up)
-#define GIT_COMMIT "5a086d065e0cc7604a77682519a78194c8cd1dc1"
-#define GIT_COMMIT_SHORT "5a086d0"
+#define GIT_COMMIT "n/a"
+#define GIT_COMMIT_SHORT "n/a"
