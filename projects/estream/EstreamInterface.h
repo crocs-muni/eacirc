8b9119587bdf92d8a8c67a7622fce3b6fde93bb0
Jiří Novotný <novotng@gmail.com>
2015-03-03 15:41:22 +0100

    Fixed internal includes of Estream & SHA-3 projects

--- projects/estream/EstreamInterface.h
+++ projects/estream/EstreamInterface.h
@@ -2,7 +2,7 @@
 #define ESTREAM_INTERFACE_H
 
 #include "EstreamConstants.h"
-#include "projects/estream/ciphers/ecrypt-portable.h"
+#include "ciphers/ecrypt-portable.h"
 
 typedef void* ECRYPT_ctx;
 
@@ -18,33 +18,33 @@ class EstreamInterface {
         virtual void ECRYPT_decrypt_bytes(void* ctx, const u8 * ciphertext, u8 * plaintext, u32 msglen) = 0;
 };
 
-#include "projects/estream/ciphers/abc/ecrypt-sync.h"
-#include "projects/estream/ciphers/achterbahn/ecrypt-sync.h"
-#include "projects/estream/ciphers/cryptmt/ecrypt-sync.h"
-#include "projects/estream/ciphers/decim/ecrypt-sync.h"
-#include "projects/estream/ciphers/dicing/ecrypt-sync.h"
-#include "projects/estream/ciphers/dragon/ecrypt-sync.h"
-#include "projects/estream/ciphers/edon80/ecrypt-sync.h"
-#include "projects/estream/ciphers/ffcsr/ecrypt-sync.h"
-#include "projects/estream/ciphers/fubuki/ecrypt-sync.h"
-#include "projects/estream/ciphers/grain/ecrypt-sync.h"
-#include "projects/estream/ciphers/hc-128/ecrypt-sync.h"
-#include "projects/estream/ciphers/hermes/ecrypt-sync.h"
-#include "projects/estream/ciphers/lex/ecrypt-sync.h"
-#include "projects/estream/ciphers/mag/ecrypt-sync.h"
-#include "projects/estream/ciphers/mickey/ecrypt-sync.h"
-#include "projects/estream/ciphers/mir-1/ecrypt-sync.h"
-#include "projects/estream/ciphers/pomaranch/ecrypt-sync.h"
-#include "projects/estream/ciphers/py/ecrypt-sync.h"
-#include "projects/estream/ciphers/rabbit/ecrypt-sync.h"
-#include "projects/estream/ciphers/salsa20/ecrypt-sync.h"
-#include "projects/estream/ciphers/sfinks/ecrypt-sync.h"
-#include "projects/estream/ciphers/sosemanuk/ecrypt-sync.h"
-#include "projects/estream/ciphers/tea/ecrypt-sync.h"
-//#include "projects/estream/ciphers/trivium/ecrypt-sync.h"     // stopped working after IDE update
-#include "projects/estream/ciphers/tsc-4/ecrypt-sync.h"
-#include "projects/estream/ciphers/wg/ecrypt-sync.h"
-//#include "projects/estream/ciphers/yamb/ecrypt-sync.h"        // stopped working after IDE update
-#include "projects/estream/ciphers/zk-crypt/ecrypt-sync.h"
+#include "ciphers/abc/ecrypt-sync.h"
+#include "ciphers/achterbahn/ecrypt-sync.h"
+#include "ciphers/cryptmt/ecrypt-sync.h"
+#include "ciphers/decim/ecrypt-sync.h"
+#include "ciphers/dicing/ecrypt-sync.h"
+#include "ciphers/dragon/ecrypt-sync.h"
+#include "ciphers/edon80/ecrypt-sync.h"
+#include "ciphers/ffcsr/ecrypt-sync.h"
+#include "ciphers/fubuki/ecrypt-sync.h"
+#include "ciphers/grain/ecrypt-sync.h"
+#include "ciphers/hc-128/ecrypt-sync.h"
+#include "ciphers/hermes/ecrypt-sync.h"
+#include "ciphers/lex/ecrypt-sync.h"
+#include "ciphers/mag/ecrypt-sync.h"
+#include "ciphers/mickey/ecrypt-sync.h"
+#include "ciphers/mir-1/ecrypt-sync.h"
+#include "ciphers/pomaranch/ecrypt-sync.h"
+#include "ciphers/py/ecrypt-sync.h"
+#include "ciphers/rabbit/ecrypt-sync.h"
+#include "ciphers/salsa20/ecrypt-sync.h"
+#include "ciphers/sfinks/ecrypt-sync.h"
+#include "ciphers/sosemanuk/ecrypt-sync.h"
+#include "ciphers/tea/ecrypt-sync.h"
+//#include "ciphers/trivium/ecrypt-sync.h"     // stopped working after IDE update
+#include "ciphers/tsc-4/ecrypt-sync.h"
+#include "ciphers/wg/ecrypt-sync.h"
+//#include "ciphers/yamb/ecrypt-sync.h"        // stopped working after IDE update
+#include "ciphers/zk-crypt/ecrypt-sync.h"
 
 #endif
