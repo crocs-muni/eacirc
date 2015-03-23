e9f4c87e76f239f6c7f4afcee10121ef0fd6e175
Jiří Novotný <novotng@gmail.com>
2015-03-23 14:54:48 +0100

    3rd party libs are now building with less restrictive warnings

---
+++ cmake/remove_cxx_flags.cmake
@@ -0,0 +1,16 @@
+cmake_minimum_required(VERSION 3.0.2)
+
+
+function(remove_cxx_flags FLAGS)
+    string(REGEX MATCHALL "-[^\ ]+" ALL_FLAGS ${CMAKE_CXX_FLAGS})
+    string(REGEX MATCHALL "-[^\ ]+" TO_REMOVE ${SUPRESS_LIBS_WARNINGS})
+
+    list (REMOVE_ITEM ALL_FLAGS ${TO_REMOVE})
+
+    foreach(FLAG ${ALL_FLAGS})
+        string(CONCAT FLAG " " ${FLAG})
+        string(CONCAT RESULT ${RESULT} ${FLAG})
+    endforeach(FLAG)
+
+    set(CMAKE_CXX_FLAGS ${RESULT})
+endfunction(remove_cxx_flags)
