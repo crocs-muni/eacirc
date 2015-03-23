3f8dcf531b10357db95ed9a6357e5c7cf7e29a48
Jiří Novotný <novotng@gmail.com>
2015-03-23 13:29:59 +0100

    Remove outdated project files since now we are using CMake

--- scripts/post-build.bat
+++
@@ -1,4 +0,0 @@
-:: requires 1 argument, current subdirectory in .\build to use
-:: usually "debug", "release", "Debug", "Release" or "."
-COPY .\build\%1\EACirc.exe .\run\EACirc.exe
-COPY .\EACirc\config.xml .\run\config.xml
