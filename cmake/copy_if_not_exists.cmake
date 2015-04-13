12c5fb9387c89c3e059f50bb6c39816eb7d67e53
xtracko <novotng@gmail.com>
2015-04-13 02:31:39 +0200

    Do not use installation process to copy runtimes to the RUN_DIR
    Now the executables are build directly to the RUN_DIR allong with
    their config files.

--- cmake/copy_if_not_exists.cmake
+++ cmake/copy_if_not_exists.cmake
@@ -1,11 +1,9 @@
 cmake_minimum_required(VERSION 3.0.2)
 
 
-function(copy_if_not_exists SRC DST)
-    if(NOT EXISTS ${DST})
-        message(STATUS "Installing: ${DST}")
-        execute_process(COMMAND ${CMAKE_COMMAND} -E copy ${SRC} ${DST})
-    else()
-        message(STATUS "Skipping: ${DST}")
-    endif()
-endfunction()
+if(NOT EXISTS ${DST})
+    message(STATUS "Installing: ${DST}")
+    execute_process(COMMAND ${CMAKE_COMMAND} -E copy ${SRC} ${DST})
+else()
+    message(STATUS "Skipping: ${DST}")
+endif()
