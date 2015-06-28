1c71a2636d38c4ab09e2d641687559989f87432c
Martin Ukrop <mukrop@mail.muni.cz>
2015-06-28 12:27:54 +0200

    [caesar] cipher files cleaup
    - parent folders according to cipher created
    - painless process thanks to sed, vim, regexp and bash :-}

---
+++ projects/caesar/aead/silc/led80n6silcv1/Measure.c.h
@@ -0,0 +1,20 @@
+#ifndef MEASURE.C_H
+#define MEASURE.C_H
+
+#include "../../../CaesarInterface.h"
+
+class Measure.c : public CaesarInterface {
+    const int maxNumRounds = -1;
+public:
+    Measure.c(int numRounds);
+    ~Measure.c();
+    int encrypt(bits_t *c, length_t *clen, const bits_t *m, length_t mlen,
+                        const bits_t *ad, length_t adlen, const bits_t *nsec, const bits_t *npub,
+                        const bits_t *k);
+    int decrypt(bits_t *m, length_t *outputmlen, bits_t *nsec,
+                        const bits_t *c, length_t clen, const bits_t *ad, length_t adlen,
+                        const bits_t *npub, const bits_t *k);
+    std::string shortDescription() const;
+};
+
+#endif // MEASURE.C_H
