a66481e213691fc7de41653a655151eda5a04ec3
Jiří Novotný <novotng@gmail.com>
2015-08-07 14:26:48 +0200

    removal of deprecated gpu_gate backend

--- eacirc/circuit/gpu_gate/Byte.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-
-using Byte = unsigned char;
--- eacirc/circuit/gpu_gate/GaCallbacks.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <GAGenome.h>
-
-
-class GaCallbacks {
-public:
-    static float evaluator(GAGenome& genome);
-};
