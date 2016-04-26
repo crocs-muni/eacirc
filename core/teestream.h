009ebce0453bf5bec2254504a8551932668bae3a
Jiří Novotný <novotng@gmail.com>
2016-04-26 22:51:27 +0200

    Code formating

--- core/teestream.h
+++ core/teestream.h
@@ -30,9 +30,7 @@ private:
             int_type const r1 = sb1->sputc(ch);
             int_type const r2 = sb2->sputc(ch);
 
-            return traits::eq_int_type(r1, eof) || traits::eq_int_type(r2, eof)
-                           ? eof
-                           : c;
+            return traits::eq_int_type(r1, eof) || traits::eq_int_type(r2, eof) ? eof : c;
         }
     }
 
@@ -45,10 +43,8 @@ template <typename char_type, typename traits = std::char_traits<char_type>>
 class BasicTeestream : public std::basic_ostream<char_type, traits> {
 public:
     BasicTeestream(
-            std::basic_ostream<char_type, traits>& o1,
-            std::basic_ostream<char_type, traits>& o2)
-        : std::basic_ostream<char_type, traits>(&tbuf)
-        , tbuf(o1.rdbuf(), o2.rdbuf()) {}
+            std::basic_ostream<char_type, traits>& o1, std::basic_ostream<char_type, traits>& o2)
+        : std::basic_ostream<char_type, traits>(&tbuf), tbuf(o1.rdbuf(), o2.rdbuf()) {}
 
 private:
     BasicTeebuf<char_type, traits> tbuf;
