89fd00bed600c98b35fe55f38c25fc07333cbb03
Jiří Novotný <novotng@gmail.com>
2016-04-26 23:50:52 +0200

    Constructor for Dataset

--- core/dataset.h
+++ core/dataset.h
@@ -4,6 +4,8 @@
 #include <vector>
 
 template <unsigned Size> struct Dataset {
+    Dataset(size_t size) : stream_A(size), stream_B(size) {}
+
     std::vector<DataVec<Size>> stream_A;
     std::vector<DataVec<Size>> stream_B;
 };
