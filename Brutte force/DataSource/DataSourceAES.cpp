df2b75ee244abcf9bf82d86e2b2f5c313d879137
Dusan Klinec <dusan.klinec@gmail.com>
2016-06-20 08:24:16 +0200

    aes gen - buffers swap

--- Brutte force/DataSource/DataSourceAES.cpp
+++ Brutte force/DataSource/DataSourceAES.cpp
@@ -20,17 +20,23 @@ long long DataSourceAES::getAvailableData() {
 
 void DataSourceAES::read(char *buffer, size_t size) {
     const int workingBufferSize = 256;
-    uint8_t workingBufferIn[workingBufferSize+16];
-    uint8_t workingBufferOu[workingBufferSize+16];
+    uint8_t workingBufferInHlp[workingBufferSize+16];
+    uint8_t workingBufferOuHlp[workingBufferSize+16];
+    uint8_t * workingBufferIn = workingBufferInHlp;
+    uint8_t * workingBufferOu = workingBufferOuHlp;
     for(int i=0; i<workingBufferSize+16; i++){
         workingBufferIn[i] = 0;
     }
 
     for(size_t offset = 0; offset < size; offset += workingBufferSize){
+        // Encrypt input buffer.
         AES128_CBC_encrypt_buffer(workingBufferOu, workingBufferIn, workingBufferSize+16, m_key, m_iv);
+        // Take last 16B (extra) as new IV for next iterations.
         memcpy(m_iv, workingBufferOu + workingBufferSize, 16);
-
+        // Copy workingBufferSizeB to the output buffer.
         memcpy(buffer+offset, workingBufferOu, std::min((size_t)workingBufferSize, size-offset));
+        // Swap input/output buffers.
+        std::swap(workingBufferIn, workingBufferOu);
     }
 }
 
