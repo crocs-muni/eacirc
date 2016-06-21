250960d6f82b8ea5848a7ed7fcfb38df6dfa1002
Dusan Klinec <dusan.klinec@gmail.com>
2016-06-21 21:43:36 +0200

    Denim added

---
+++ Brutte force/DataSource/DataSourceDecim.cpp
@@ -0,0 +1,50 @@
+//
+// Created by Dusan Klinec on 21.06.16.
+//
+
+#include "DataSourceDecim.h"
+#define DECIM_DEFAULT_IV_LEN 16
+#define DECIM_DEFAULT_KEY_LEN 16
+
+DataSourceDecim::DataSourceDecim(unsigned long seed, int rounds) {
+    this->m_gen = new std::minstd_rand((unsigned int) seed);
+    this->m_cipher = new ECRYPT_Decim();
+    this->m_cipher->numRounds = rounds;
+    this->m_cipher->ECRYPT_init();
+
+    uint8_t keyBuff[DECIM_DEFAULT_KEY_LEN];
+    uint8_t ivBuff[DECIM_DEFAULT_IV_LEN];
+    for (unsigned char i = 0; i < DECIM_DEFAULT_KEY_LEN; i++) {
+        keyBuff[i] = (uint8_t) this->m_gen->operator()();
+    }
+    for (unsigned char i = 0; i < DECIM_DEFAULT_IV_LEN; i++) {
+        ivBuff[i] = (uint8_t) this->m_gen->operator()();
+    }
+
+    this->m_cipher->ECRYPT_keysetup(&this->m_ctx, keyBuff, (u32)DECIM_DEFAULT_KEY_LEN*8, (u32)DECIM_DEFAULT_IV_LEN*8);
+    this->m_cipher->ECRYPT_ivsetup(&this->m_ctx, ivBuff);
+}
+
+DataSourceDecim::~DataSourceDecim() {
+    if (this->m_gen != nullptr) {
+        delete this->m_gen;
+        this->m_gen = nullptr;
+    }
+
+    if (this->m_cipher != nullptr){
+        delete this->m_cipher;
+        this->m_cipher = nullptr;
+    }
+}
+
+long long DataSourceDecim::getAvailableData() {
+    return 9223372036854775807; // 2^63-1
+}
+
+void DataSourceDecim::read(char *buffer, size_t size) {
+    this->m_cipher->DECIM_keystream_bytes(&this->m_ctx, (u8*)buffer, (u32)size);
+}
+
+std::string DataSourceDecim::desc() {
+    return "Decim";
+}
---
+++ Brutte force/DataSource/DataSourceDecim.h
@@ -0,0 +1,31 @@
+//
+// Created by Dusan Klinec on 21.06.16.
+//
+
+#ifndef BRUTTE_FORCE_DATASOURCEDECIM_H
+#define BRUTTE_FORCE_DATASOURCEDECIM_H
+
+#include <random>
+#include "../DataGenerators/estream/EstreamInterface.h"
+#include "../DataGenerators/estream/ciphers/decim/ecrypt-sync.h"
+#include "DataSource.h"
+
+class DataSourceDecim : public DataSource{
+public:
+    DataSourceDecim(unsigned long seed = 0, int rounds = 8);
+    ~DataSourceDecim();
+
+    virtual long long getAvailableData() override;
+    virtual void read(char *buffer, size_t size) override;
+    virtual std::string desc() override;
+
+protected:
+    std::minstd_rand * m_gen;
+    DECIM_ctx m_ctx;
+    ECRYPT_Decim * m_cipher;
+private:
+
+};
+
+
+#endif //BRUTTE_FORCE_DATASOURCEDECIM_H
