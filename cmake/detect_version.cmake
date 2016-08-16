d0796e890b4694a772f2aed67ee795f759febadc
Jiří Novotný <novotng@gmail.com>
2016-08-16 13:45:22 +0200

    Versioning of eacirc updated
    A version of eacirc is now an output of 'git describe --tags --dirty'

--- cmake/detect_version.cmake
+++ cmake/detect_version.cmake
@@ -3,22 +3,15 @@ find_package(Git)
 function(detect_version FILE)
     if(GIT_FOUND)
         execute_process(
-            COMMAND ${GIT_EXECUTABLE} log -1 --format=%H
+            COMMAND ${GIT_EXECUTABLE} describe --tags --dirty
             WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-            OUTPUT_VARIABLE GIT_COMMIT
+            OUTPUT_VARIABLE VERSION_TAG
             OUTPUT_STRIP_TRAILING_WHITESPACE
-        )
-        execute_process(
-            COMMAND ${GIT_EXECUTABLE} log -1 --format=%h
-            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-            OUTPUT_VARIABLE GIT_COMMIT_SHORT
-            OUTPUT_STRIP_TRAILING_WHITESPACE
-        )
-    else()
+            )
+    else(${VERSION_TAG})
         message(WARNING "Git not found")
-        set(GIT_COMMIT n/a)
-        set(GIT_COMMIT_SHORT n/a)
+        set(GIT_COMMIT "n/a")
+        set(GIT_COMMIT_SHORT "n/a")
     endif()
     configure_file("${CMAKE_CURRENT_SOURCE_DIR}/${FILE}.in" "${CMAKE_CURRENT_SOURCE_DIR}/${FILE}" @ONLY)
-    MESSAGE("Successfully updated commit information in ${FILE}.")
 endfunction(detect_version)
