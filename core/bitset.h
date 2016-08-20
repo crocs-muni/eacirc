510ec0929164d58b0dea18181e3fcad6d5627f29
Jiří Novotný <novotng@gmail.com>
2016-08-20 13:52:45 +0200

    Some fixes to bitset

--- core/bitset.h
+++ core/bitset.h
@@ -14,7 +14,7 @@ int count_trailing_zeros(std::uint64_t x) {
 #elif _MSC_VER
     return __lzcnt64(x);
 #elif __CUDACC__
-    return __ffsll(*reinterpret_cast<i64 *>(&x)) - 1;
+    return __ffsll(*reinterpret_cast<i64*>(&x)) - 1;
 #else
 #warning "CAUTION! Unsupported compiler! Slow trivial implmentation is used"
     if (x == 0)
@@ -50,7 +50,7 @@ int count_trailing_zeros(std::uint32_t x) {
 #elif _MSC_VER
     return __lzcnt(x);
 #elif __CUDACC__
-    return __ffs(*reinterpret_cast<i32 *>(&x)) - 1;
+    return __ffs(*reinterpret_cast<i32*>(&x)) - 1;
 #else
 #warning "CAUTION! Unsupported compiler! Slow trivial implmentation is used"
     if (x == 0)
@@ -76,23 +76,84 @@ int count_trailing_zeros(std::uint32_t x) {
 #endif
 }
 
+int count_trailing_zeros(std::uint16_t x) {
+    return count_trailing_zeros(static_cast<std::uint32_t>(x));
+}
+
+int count_trailing_zeros(std::uint8_t x) {
+    return count_trailing_zeros(static_cast<std::uint32_t>(x));
+}
+
 } // namespace _impl
 
 /**
  * @brief bitset using the smallest space that is possible
  */
 template <unsigned Bits> struct bitset {
+    // clang-format off
     using value_type = choose_t<opt<Bits <= 8, std::uint8_t>,
                                 opt<Bits <= 16, std::uint16_t>,
                                 opt<Bits <= 32, std::uint32_t>,
                                 opt<Bits <= 64, std::uint64_t>>;
-    template <unsigned> friend struct true_bit_iterator;
+    // clang-format on
+
+    /**
+     * @brief iterate over true bits in \a bitset and get their position
+     */
+    struct true_bit_iterator {
+        using value_type = const unsigned;
+        using reference = value_type&;
+        using pointer = value_type*;
+        using iterator_category = std::forward_iterator_tag;
+
+        true_bit_iterator()
+            : _value(0u) {
+        }
+
+        explicit true_bit_iterator(bitset container)
+            : _value(container._value) {
+        }
+
+        true_bit_iterator& operator++() {
+            auto i = _impl::count_trailing_zeros(_value);
+            _value ^= (1u << i);
+            return *this;
+        }
+
+        true_bit_iterator operator++(int) {
+            auto self = *this;
+            ++(*this);
+            return self;
+        }
+
+        unsigned operator*() const {
+            return static_cast<unsigned>(_impl::count_trailing_zeros(_value));
+        }
+
+        bool operator==(true_bit_iterator b) const {
+            return _value == b._value;
+        }
+
+        bool operator!=(true_bit_iterator b) const {
+            return !(*this == b);
+        }
+
+    private:
+        bitset::value_type _value;
+    };
 
     bitset()
-        : _value(0u) {}
+        : _value(0u) {
+    }
 
     explicit bitset(value_type value)
-        : _value(value) {}
+        : _value(value) {
+    }
+
+    bitset& operator=(value_type value) {
+        _value = value;
+        return *this;
+    }
 
     void set(unsigned i) {
         ASSERT(i < Bits);
@@ -114,48 +175,24 @@ template <unsigned Bits> struct bitset {
         return _value & (1u << i);
     }
 
-    bool operator==(bitset b) const { return _value == b._value; }
-    bool operator!=(bitset b) const { return !(*this == b); }
+    bool operator==(bitset b) const {
+        return _value == b._value;
+    }
+
+    bool operator!=(bitset b) const {
+        return !(*this == b);
+    }
+
+    true_bit_iterator begin() const {
+        return true_bit_iterator{*this};
+    }
+
+    true_bit_iterator end() const {
+        return true_bit_iterator{};
+    }
 
 private:
     value_type _value;
-};
-
-/**
- * @brief iterate over true bits in \a bitset and get their position
- */
-template <unsigned Bits>
-struct true_bit_iterator
-        : std::iterator<std::forward_iterator_tag, const unsigned> {
-    true_bit_iterator()
-        : _container(0u) {}
-
-    explicit true_bit_iterator(bitset<Bits> container)
-        : _container(container) {}
-
-    true_bit_iterator &operator++() {
-        auto i = _impl::count_trailing_zeros(_container._value);
-        _container ^= (1u << i);
-        return *this;
-    }
-
-    true_bit_iterator operator++(int) {
-        auto self = *this;
-        ++(*this);
-        return self;
-    }
-
-    unsigned operator*() const {
-        return static_cast<unsigned>(_impl::count_trailing_zeros(_container));
-    }
-
-    bool operator==(true_bit_iterator b) const {
-        return _container == b._container;
-    }
-    bool operator!=(true_bit_iterator b) const { return !(*this == b); }
-
-private:
-    bitset<Bits> _container;
 };
 
 } // namespace core
