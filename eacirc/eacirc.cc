0a413367aa4a00685e21c9ce0a4251fe1b140e0d
Jiří Novotný <novotng@gmail.com>
2016-08-20 14:16:05 +0200

    Application main logic

--- eacirc/eacirc.cc
+++ eacirc/eacirc.cc
@@ -1,11 +1,27 @@
 #include "eacirc.h"
 #include <fstream>
 
-eacirc::eacirc(std::string settings)
-    : eacirc(std::ifstream(settings)) {}
+eacirc::eacirc(std::string config)
+    : eacirc(std::ifstream(config)) {
+}
 
-eacirc::eacirc(std::istream &settings)
-    : _settings(core::json::parse(settings))
-    , _seed(seed<std::uint64_t>::create(_settings["main"]["seed"])) {}
+eacirc::eacirc(std::istream& config)
+    : _config(core::json::parse(config))
+    , _seed(seed<std::uint64_t>::create(_config["seed"]))
+    , _seed_source(_seed.value())
+    , _num_of_epochs(_config["num-of-epochs"])
+    , _significance_level(_config["significance-level"]) {
+}
 
-void eacirc::run() {}
+void eacirc::run() {
+    std::vector<double> pvalues;
+    pvalues.reserve(_num_of_epochs);
+
+    for (std::size_t i = 0; i != _num_of_epochs; ++i) {
+        auto pvalue = _backend->train();
+
+        pvalues.emplace_back(pvalue);
+    }
+
+    // TODO: ks_test(pvalues, _significance_level);
+}
--- eacirc/eacirc.h
+++ eacirc/eacirc.h
@@ -1,23 +1,32 @@
 #pragma once
 
+#include "backend.h"
 #include "seed.h"
 #include <core/json.h>
+#include <core/random.h>
 #include <core/stream.h>
 #include <memory>
 
 struct eacirc {
-    eacirc(std::string settings);
+    eacirc(std::string config);
 
-    eacirc(std::istream &settings);
-    eacirc(std::istream &&settings)
-        : eacirc(settings) {}
+    eacirc(std::istream& config);
+    eacirc(std::istream&& config)
+        : eacirc(config) {
+    }
 
     void run();
 
 private:
-    const core::json _settings;
-    const seed<std::uint64_t> _seed;
+    const core::json _config;
 
+    const seed<std::uint64_t> _seed;
+    core::main_seed_source _seed_source;
+
+    const std::size_t _num_of_epochs;
+    const std::size_t _significance_level;
+
+    std::unique_ptr<backend> _backend;
     std::unique_ptr<core::stream> _stream_a;
     std::unique_ptr<core::stream> _stream_b;
 };
