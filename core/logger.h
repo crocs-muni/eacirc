2d33e96b238c07d92267d04c7a0f871c3e3779a4
Jiří Novotný <novotng@gmail.com>
2016-09-04 13:58:08 +0200

    Logger updated

--- core/logger.h
+++ core/logger.h
@@ -3,18 +3,14 @@
 #include <ostream>
 
 struct logger {
-    static void warning(std::string msg) {
-        logger::out() << "[warning] " << msg << std::endl;
-    }
+    static void warning(std::string msg) { logger::warning() << msg << std::endl; }
+    static void error(std::string msg) { logger::error() << msg << std::endl; }
+    static void info(std::string msg) { logger::info() << msg << std::endl; }
 
-    static void error(std::string msg) {
-        logger::out() << "[error] " << msg << std::endl;
-    }
-
-    static void info(std::string msg) {
-        logger::out() << "[info] " << msg << std::endl;
-    }
+    static std::ostream& warning() { return logger::out() << "[warning] "; }
+    static std::ostream& error() { return logger::out() << "[error] "; }
+    static std::ostream& info() { return logger::out() << "[info] "; }
 
 private:
-    static std::ostream &out();
+    static std::ostream& out();
 };
