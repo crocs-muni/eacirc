319c6d3c702c5f0df8ae90b7f880a998a41197d8
Jiří Novotný <novotng@gmail.com>
2016-09-05 12:55:02 +0200

    Main computation loop added

--- eacirc/eacirc.cc
+++ eacirc/eacirc.cc
@@ -1,27 +1,72 @@
 #include "eacirc.h"
+#include "statistics.h"
+#include <core/logger.h>
+#include <core/random.h>
 #include <fstream>
+#include <pcg/pcg_random.hpp>
 
-eacirc::eacirc(std::string config)
-    : eacirc(std::ifstream(config)) {
+#include "circuit/backend.h"
+#include "streams/filestream.h"
+
+static std::ifstream open_config_file(std::string path) {
+    std::ifstream file(path);
+    if (!file.is_open())
+        throw std::runtime_error("can't open config file " + path);
+    return file;
 }
 
-eacirc::eacirc(std::istream& config)
-    : _config(core::json::parse(config))
-    , _seed(seed<std::uint64_t>::create(_config["seed"]))
-    , _seed_source(_seed.value())
-    , _num_of_epochs(_config["num-of-epochs"])
-    , _significance_level(_config["significance-level"]) {
+eacirc::eacirc(std::string config)
+    : eacirc(open_config_file(config)) {}
+
+eacirc::eacirc(json const& config)
+    : _config(config)
+    , _seed(seed::create(config["seed"]))
+    , _num_of_epochs(config["num-of-epochs"])
+    , _significance_level(config["significance-level"])
+    , _tv_size(config["tv-size"])
+    , _tv_count(config["tv-count"]) {
+    seed_seq_from<pcg32> main_seeder(_seed);
+
+    // TODO: create streams & backend
+    _stream_a = std::make_unique<streams::filestream>(config["stream-a"]);
+    _stream_b = std::make_unique<streams::filestream>(config["stream-b"]);
+
+    _backend = circuit::create_backend(_tv_size, config["backend"], main_seeder);
 }
 
 void eacirc::run() {
     std::vector<double> pvalues;
     pvalues.reserve(_num_of_epochs);
 
-    for (std::size_t i = 0; i != _num_of_epochs; ++i) {
-        auto pvalue = _backend->train();
+    dataset a{_tv_size, _tv_count};
+    dataset b{_tv_size, _tv_count};
 
-        pvalues.emplace_back(pvalue);
+    _stream_a->read(a);
+    _stream_b->read(b);
+
+    for (std::size_t i = 0; i != _num_of_epochs; ++i) {
+        _backend->train(a, b);
+
+        _stream_a->read(a);
+        _stream_b->read(b);
+
+        pvalues.emplace_back(_backend->test(a, b));
+
+        std::cout << i << std::endl;
     }
 
-    // TODO: ks_test(pvalues, _significance_level);
+    ks_uniformity_test test{pvalues, _significance_level};
+
+    logger::info() << "KS test on p-values of size " << pvalues.size() << std::endl;
+    logger::info() << "KS statistics: " << test.test_statistic << std::endl;
+    logger::info() << "KS critical value: " << _significance_level << "%: " << test.critical_value
+                   << std::endl;
+
+    if (test.test_statistic > test.critical_value) {
+        logger::info() << "KS is in " << _significance_level
+                       << "% interval -> uniformity hypothesis rejected" << std::endl;
+    } else {
+        logger::info() << "KS is not in " << _significance_level
+                       << "% interval -> uniformity hypothesis not rejected" << std::endl;
+    }
 }
--- eacirc/eacirc.h
+++ eacirc/eacirc.h
@@ -3,30 +3,35 @@
 #include "backend.h"
 #include "seed.h"
 #include <core/json.h>
-#include <core/random.h>
 #include <core/stream.h>
 #include <memory>
 
 struct eacirc {
-    eacirc(std::string config);
+    eacirc(std::string cofig);
 
-    eacirc(std::istream& config);
+    eacirc(std::istream& config)
+        : eacirc(json::parse(config)) {}
+
     eacirc(std::istream&& config)
-        : eacirc(config) {
-    }
+        : eacirc(json::parse(config)) {}
+
+    eacirc(json&& config)
+        : eacirc(config) {}
+
+    eacirc(json const& config);
 
     void run();
 
 private:
-    const core::json _config;
+    const json _config;
+    const seed _seed;
 
-    const seed<std::uint64_t> _seed;
-    core::main_seed_source _seed_source;
-
-    const std::size_t _num_of_epochs;
-    const std::size_t _significance_level;
+    const std::uint64_t _num_of_epochs;
+    const unsigned _significance_level;
+    const unsigned _tv_size;
+    const std::uint64_t _tv_count;
 
     std::unique_ptr<backend> _backend;
-    std::unique_ptr<core::stream> _stream_a;
-    std::unique_ptr<core::stream> _stream_b;
+    std::unique_ptr<stream> _stream_a;
+    std::unique_ptr<stream> _stream_b;
 };
