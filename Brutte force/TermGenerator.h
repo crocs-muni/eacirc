aff41e4b5bed76a55127889786285fc47b6f0e5a
Dusan Klinec <dusan.klinec@gmail.com>
2016-10-03 09:02:31 +0200

    comment note on element minimality for heaps

--- Brutte force/TermGenerator.h
+++ Brutte force/TermGenerator.h
@@ -137,6 +137,8 @@ double computeTopKInPlace(std::vector<bitarray<u64> * > a,
         zscore = fabs(CommonFnc::zscore((double)freqOnes/numTVs,(double)refCount/numTVs,numTVs));
         //std::cout  << indices.size() << " " << freqOnes << " " << zscore << std::endl;
         // If queue is not full OR the value is higher than queue-minimal, add it.
+        // Note: Minheap has its minimum pointed by front().
+        // But push_selsort has it in back(). Change if you change the implementation.
         const pairZscoreTerm & c_top = queue.front();
         if (zscore > c_top.first){
             pairZscoreTerm c_pair(zscore, indices);
