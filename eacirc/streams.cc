9d67198883cec59a56653e370171f429e9c04393
Jiří Novotný <novotng@gmail.com>
2016-10-27 17:40:38 +0200

    Bugfix of false-stream

--- eacirc/streams.cc
+++ eacirc/streams.cc
@@ -90,7 +90,7 @@ std::unique_ptr<stream> make_stream(json const& config, default_seed_source& see
     else if (type == "true-stream")
         return std::make_unique<true_stream>();
     else if (type == "false-stream")
-        return std::make_unique<true_stream>();
+        return std::make_unique<false_stream>();
     else if (type == "mt19937-stream")
         return std::make_unique<mt19937_stream>(seeder);
     else if (type == "pcg32-stream")
