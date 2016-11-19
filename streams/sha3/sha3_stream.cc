349dd8d780c815c3d8610ec4c2102448765fdfed
Karel Kubicek <karel.kubicek@mail.muni.cz>
2016-11-19 16:12:03 +0100

    hash_size bits -> bytes, readability

--- streams/sha3/sha3_stream.cc
+++ streams/sha3/sha3_stream.cc
@@ -9,7 +9,7 @@ static void
 hash_data(sha3_interface& hasher, const I& data, std::uint8_t* hash, const std::size_t hash_size) {
     using std::to_string;
 
-    int status = hasher.Init(int(hash_size));
+    int status = hasher.Init(int(hash_size * 8));
     if (status != 0)
         throw std::runtime_error("cannot initialize hash (code: " + to_string(status) + ")");
 
@@ -26,14 +26,13 @@ sha3_stream::sha3_stream(const json& config, std::size_t osize)
     : stream(osize)
     , _algorithm(config.at("algorithm").get<std::string>())
     , _round(config.at("round"))
-    , _hash_size(std::size_t(config.at("hash-bitsize")))
-    , _source(make_stream(config.at("source"), _hash_size / 8)) // TODO: hash-input-size?
+    , _hash_size(std::size_t(config.at("hash-bitsize")) / 8)
+    , _source(make_stream(config.at("source"), _hash_size)) // TODO: hash-input-size?
     , _hasher(sha3_factory::create(_algorithm, unsigned(_round)))
-    , _data(((_hash_size / 8) > osize)
-                    ? (_hash_size / 8)
-                    : ((_hash_size / 8) % osize)
-                              ? ((osize / (_hash_size / 8)) + 1) * (_hash_size / 8)
-                              : osize) { // round osize to multiple of _hash_size
+    , _data((_hash_size > osize)
+                    ? _hash_size
+                    : (_hash_size % osize) ? ((osize / _hash_size) + 1) * _hash_size
+                                           : osize) { // round osize to multiple of _hash_size
 
     if ((std::size_t(config.at("hash-bitsize")) % 8) != 0)
         throw std::runtime_error("the SHA-3 hash-bitsize parameter must be multiple of 8");
@@ -44,7 +43,7 @@ sha3_stream::~sha3_stream() = default;
 
 vec_view sha3_stream::next() {
 
-    for (auto beg = _data.begin(); beg != _data.end(); beg += (_hash_size / 8)) {
+    for (auto beg = _data.begin(); beg != _data.end(); beg += _hash_size) {
         vec_view view = _source->next();
 
         hash_data(*_hasher, view, &(*beg), _hash_size);
